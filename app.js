2e4794038f385f23016014b68a9bfc134176d8e1
Jorge Veliz <javierf.1995@gmail.com>
2020-09-29 17:56:20 -0600

    pagina

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ app.use(bodyParser.urlencoded({
   extended: false
 }))
 app.use(bodyParser.json())
+var path = require('path')
+app.use(express.static(path.join(__dirname, './public')))
+
 /**
  *
  * Description. (peticion inicial)
